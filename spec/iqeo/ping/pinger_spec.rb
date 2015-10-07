357f646a53537383c76deafd81c284b0dba5e9ad
Gerard Fowley <gerard.fowley@iqeo.net>
2015-10-07 09:05:46 -0400

    tidy spec

--- spec/iqeo/ping/pinger_spec.rb
+++ spec/iqeo/ping/pinger_spec.rb
@@ -4,36 +4,45 @@ RSpec::Matchers.define_negated_matcher :not_be_alive, :be_alive
 
 describe Iqeo::Ping::Pinger do
 
-  before( :all ) { `iptables -I INPUT -d 127.0.0.252/30 -j DROP` }
+  HOST_SPEC           = '127.0.0.1'
+  ALL_HOSTS_SPEC      = '127.0.0.1-3,252-254'
+  FAST_HOSTS          = [ '127.0.0.1','127.0.0.2','127.0.0.3' ]
+  FAST_HOSTS_SPEC     = '127.0.0.1-3'
+  SLOW_HOSTS          = [ '127.0.0.252','127.0.0.253','127.0.0.254' ]
+  SLOW_HOSTS_SPEC     = '127.0.0.252-254'
+  SLOW_HOSTS_IPTABLES = '127.0.0.252/30'
+  RESULT_KEYS         = [ :ip, :ping, :time, :exception ]
+
+  before( :all ) { `iptables -I INPUT -d #{SLOW_HOSTS_IPTABLES} -j DROP` }
 
   context '.new' do
 
     context 'with defaults' do
-      it( 'accepts an IP address string' ) { expect { Iqeo::Ping::Pinger.new '127.0.0.1' }.to_not raise_error }
-      it( 'accepts a Hostspec' )           { expect { Iqeo::Ping::Pinger.new( Iqeo::Hostspec::Hostspec.new '127.0.0.1' ) }.to_not raise_error }
+      it( 'accepts an IP address string' ) { expect { Iqeo::Ping::Pinger.new HOST_SPEC }.to_not raise_error }
+      it( 'accepts a Hostspec' )           { expect { Iqeo::Ping::Pinger.new( Iqeo::Hostspec::Hostspec.new HOST_SPEC ) }.to_not raise_error }
       it( 'requires an argument' )         { expect { Iqeo::Ping::Pinger.new }.to raise_error ArgumentError }
     end
 
     context 'with specifics' do
      
-      it ( 'accepts timeout' )  { expect( Iqeo::Ping::Pinger.new( '127.0.0.1', timeout: 99).timeout ).to eq 99 }
+      it ( 'accepts timeout' )  { expect( Iqeo::Ping::Pinger.new( HOST_SPEC, timeout: 99).timeout ).to eq 99 }
 
       context 'ICMP' do
-        before( :all )            { @pinger = Iqeo::Ping::Pinger.new '127.0.0.1', protocol: :icmp, timeout: 99 }
+        before( :all )            { @pinger = Iqeo::Ping::Pinger.new HOST_SPEC, protocol: :icmp, timeout: 99 }
         it ( 'accepts protocol' ) { expect( @pinger.protocol ).to eq :icmp }
         it ( 'sets ping class' )  { expect( @pinger.klass ).to eq Net::Ping::ICMP }
         it ( 'sets port' )        { expect( @pinger.port ).to be nil }
       end
 
       context 'TCP' do
-        before( :all )            { @pinger = Iqeo::Ping::Pinger.new '127.0.0.1', protocol: :tcp, timeout: 99 }
+        before( :all )            { @pinger = Iqeo::Ping::Pinger.new HOST_SPEC, protocol: :tcp, timeout: 99 }
         it ( 'accepts protocol' ) { expect( @pinger.protocol ).to eq :tcp }
         it ( 'sets ping class' )  { expect( @pinger.klass ).to eq Net::Ping::TCP }
         it ( 'sets port' )        { expect( @pinger.port ).to be 80 }
       end
 
       context 'UDP' do
-        before( :all )            { @pinger = Iqeo::Ping::Pinger.new '127.0.0.1', protocol: :udp, timeout: 99 }
+        before( :all )            { @pinger = Iqeo::Ping::Pinger.new HOST_SPEC, protocol: :udp, timeout: 99 }
         it ( 'accepts protocol' ) { expect( @pinger.protocol ).to eq :udp }
         it ( 'sets ping class' )  { expect( @pinger.klass ).to eq Net::Ping::UDP }
         it ( 'sets port' )        { expect( @pinger.port ).to be 53 }
@@ -44,8 +53,8 @@ describe Iqeo::Ping::Pinger do
   end
 
   context '#hostspec' do
-    it( 'is from string' )      { expect( Iqeo::Ping::Pinger.new('127.0.0.1').hostspec ).to be_a Iqeo::Hostspec::Hostspec }
-    it( 'is passed from .new' ) { expect( Iqeo::Ping::Pinger.new( Iqeo::Hostspec::Hostspec.new '127.0.0.1' ).hostspec ).to be_a Iqeo::Hostspec::Hostspec }
+    it( 'is from string' )      { expect( Iqeo::Ping::Pinger.new(HOST_SPEC).hostspec ).to be_a Iqeo::Hostspec::Hostspec }
+    it( 'is passed from .new' ) { expect( Iqeo::Ping::Pinger.new( Iqeo::Hostspec::Hostspec.new HOST_SPEC ).hostspec ).to be_a Iqeo::Hostspec::Hostspec }
   end
 
   context '.classes' do
@@ -55,7 +64,7 @@ describe Iqeo::Ping::Pinger do
   end
 
   context 'defaults' do
-    before( :all )     { @pinger = Iqeo::Ping::Pinger.new '127.0.0.1' }
+    before( :all )     { @pinger = Iqeo::Ping::Pinger.new HOST_SPEC }
     it( '#protocol' )  { expect( @pinger.protocol ).to eq Iqeo::Ping::Pinger::PROTOCOL }
     it( '#port' )      { expect( @pinger.port ).to eq Iqeo::Ping::Pinger::PORT }
     it( '#timeout' )   { expect( @pinger.timeout ).to eq Iqeo::Ping::Pinger::TIMEOUT }
@@ -63,7 +72,7 @@ describe Iqeo::Ping::Pinger do
 
   context 'short scan' do
 
-    before( :all ) { @pinger = Iqeo::Ping::Pinger.new '127.0.0.1-3' }
+    before( :all ) { @pinger = Iqeo::Ping::Pinger.new FAST_HOSTS_SPEC }
 
     it( '#start' ) do
       expect { @pinger.start }.to_not raise_error
@@ -86,15 +95,15 @@ describe Iqeo::Ping::Pinger do
     end
 
     context '#results' do
-      it( 'expected keys' ) { expect( @pinger.results ).to all( include :ip, :ping, :time, :exception ) }
-      it( 'for each host' ) { expect( @pinger.results.collect { |r| r[:ip] } ).to eq [ '127.0.0.1','127.0.0.2','127.0.0.3' ] }
+      it( 'expected keys' ) { expect( @pinger.results ).to all( include *RESULT_KEYS ) }
+      it( 'for each host' ) { expect( @pinger.results.collect { |r| r[:ip] } ).to eq FAST_HOSTS }
     end
 
   end
 
   context 'long scan' do
 
-    before( :all ) { @pinger = Iqeo::Ping::Pinger.new '127.0.0.252-254' }
+    before( :all ) { @pinger = Iqeo::Ping::Pinger.new SLOW_HOSTS_SPEC }
 
     it( '#start' ) do
       expect { @pinger.start }.to_not raise_error
@@ -114,15 +123,15 @@ describe Iqeo::Ping::Pinger do
     end
 
     context '#results' do
-      it( 'expected keys' )  { expect( @pinger.results ).to all( include :ip, :ping, :time, :exception ) }
-      it( 'for each host' )  { expect( @pinger.results.collect { |r| r[:ip] } ).to eq [ '127.0.0.252','127.0.0.253','127.0.0.254' ] }
+      it( 'expected keys' )  { expect( @pinger.results ).to all( include *RESULT_KEYS ) }
+      it( 'for each host' )  { expect( @pinger.results.collect { |r| r[:ip] } ).to eq SLOW_HOSTS }
     end
 
   end
 
   context 'stop scan' do
 
-    before( :all ) { @pinger = Iqeo::Ping::Pinger.new '127.0.0.1-3,252-254' }
+    before( :all ) { @pinger = Iqeo::Ping::Pinger.new ALL_HOSTS_SPEC }
 
     it( '#start' ) do
       expect { @pinger.start }.to_not raise_error
@@ -146,8 +155,8 @@ describe Iqeo::Ping::Pinger do
 
     context '#results' do
       context 'for complete hosts' do
-        it( 'have expected keys' ) { expect( @pinger.results[0..2] ).to all( include :ip, :ping, :time, :exception ) }
-        it( 'for each host' )      { expect( @pinger.results[0..2].collect { |r| r[:ip] } ).to eq [ '127.0.0.1','127.0.0.2','127.0.0.3' ] }
+        it( 'have expected keys' ) { expect( @pinger.results[0..2] ).to all( include *RESULT_KEYS ) }
+        it( 'for each host' )      { expect( @pinger.results[0..2].collect { |r| r[:ip] } ).to eq FAST_HOSTS }
       end
       context 'for incomplete hosts' do
         it( 'are nil' )            { expect( @pinger.results[3..5] ).to all( be_nil ) }
@@ -156,7 +165,7 @@ describe Iqeo::Ping::Pinger do
 
   end
 
-  after( :all ) { `iptables -D INPUT -d 127.0.0.252/30 -j DROP` }
+  after( :all ) { `iptables -D INPUT -d #{SLOW_HOSTS_IPTABLES} -j DROP` }
 
 end
 
